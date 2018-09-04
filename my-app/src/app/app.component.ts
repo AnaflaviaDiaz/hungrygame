447b8b87a7db3b1148ce380ce376a0d37e1681f0
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-09-04 18:02:19 -0500

    configurando angular

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My First Angular App! Anaflavia y Yuli';
+}
+
+// https://stackblitz.com/angular/xkxkdjbqaol?file=src%2Fapp%2Fhero-search%2Fhero-search.component.ts
