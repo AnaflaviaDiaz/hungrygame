c04edb4ae4ad64712b2ea3e85462ae5c4d3fd40e
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-09-07 15:14:55 -0500

    login-signup view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { WallComponent } from './wall/wall.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    WallComponent
   ],
   imports: [
     BrowserModule
