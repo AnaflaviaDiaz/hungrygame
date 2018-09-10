271014a8d3cdfacba101d4ae9997763e2e6ea685
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-09-10 18:10:12 -0500

    logout visual con direcciones de login y signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 NgModule({
